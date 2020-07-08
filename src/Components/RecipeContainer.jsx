279672052ab197a7fdf5afcfa0c83304bd900d17
jameschjiang13 <monochrome473@gmail.com>
2020-07-08 17:42:15 -0400

    adding form fetch submit

--- src/Components/RecipeContainer.jsx
+++ src/Components/RecipeContainer.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import RecipeCard from './RecipeCard'
 
 export default function RecipeContainer(props) {
-    console.log(props)
     let componentArr = props.recipes.map((recipe) => {
         return <RecipeCard ingredients={recipe.ingredients} instructions={recipe.instructions}/>
     }
--- src/Components/RecipeForm.jsx
+++ src/Components/RecipeForm.jsx
@@ -1,28 +1,109 @@
-import React, { Component } from "react";
+// import React from 'react'
+import React, {useState, Children} from 'react'
 
-class RecipeForm extends Component {
-  state = {
-    ingredients: "",
-    instructions: "",
-  };
+export default function RecipeForm(props) {
 
-  render() {
-    return (
+  let [ingredients, setIngredients] = useState("")
+  let [instructions, setInstructions] = useState("")
+
+  let dropDownOptions = props.foodArr.map((food) => {
+      return <option value={food.id}>{food.name}</option>
+    })
+
+  let handleSubmit = (e) => {
+    e.preventDefault()
+    let recipeObj = {
+      dish_id: e.target.dish.value,
+      ingredients: e.target.ingredients.value,
+      instructions: e.target.instructions.value
+    } 
+    console.log(recipeObj)
+    fetch("http://localhost:4000/recipes", {
+      method: "POST",
+      headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(recipeObj)
+        })
+        .then(resp => resp.json())
+        .then(console.log)
+    }
+  
+  return (
+    <div>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h3>Add A Recipe To This Dish:</h3>
+          <label>Meal</label> <br />
+          <textarea name="meal" type="text" value = {props.mealName} />
+          <br/>
+          <label>Dish</label> <br />
+          <select name="dish">
+            {dropDownOptions}
+          </select>
+          <br/>
           <label>Ingredients:</label> <br />
-          <textarea name="ingredients" type="text" />
+          <textarea name="ingredients" type="text" value = {ingredients} onChange={(e) => setIngredients(e.target.value)} />
           <br />
           <label>Instructions:</label>
           <br />
-          <textarea name="instructions" type="textarea" />
+          <textarea name="instructions" type="text" value = {instructions} onChange={(e) => setInstructions(e.target.value)}/>
           <br />
           <button>Submit Recipe</button>
         </form>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default RecipeForm;
+
+// import React, { Component } from "react";
+
+// class RecipeForm extends Component {
+
+//   handleInput = (e) => {
+//     console.log(e)
+//     console.log(e.target.name)
+//   }
+
+//   dropDownOptions = (foodArr) => {
+//     foodArr.map((food) => {
+//       return <option value="food.name">{food.name}</option>
+//     }
+//     )
+//   }
+  
+  
+
+//   render() {
+//     return (
+//       <div>
+//         <form>
+//           <h3>Add A Recipe To This Dish:</h3>
+//           <label>Meal</label> <br />
+//           <textarea name="meal" type="text" value = {this.props.mealName} />
+//           <br/>
+//           <label>Dish</label> <br />
+//           <select>
+//             {this.dropDownOptions(this.props.foodArr)}
+//           {/* <option value="grapefruit">Grapefruit</option>
+//           <option value="lime">Lime</option>
+//           <option selected value="coconut">Coconut</option>
+//           <option value="mango">Mango</option> */}
+//           </select>
+//           <br/>
+//           <label>Ingredients:</label> <br />
+//           <textarea name="ingredients" type="text" value = "hoho" onChange={this.handleInput} />
+//           <br />
+//           <label>Instructions:</label>
+//           <br />
+//           <textarea name="instructions" type="textarea" value = "hihi" onChange={this.handleInput}/>
+//           <br />
+//           <button>Submit Recipe</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default RecipeForm;
