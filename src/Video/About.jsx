6ae7af2afe9593a5701a2441f9940c086a6ca5da
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-07-08 17:18:21 -0400

    about

---
+++ src/Video/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Monster Hunter World</h2>
+      <img
+        src="https://cdn-ext.fanatical.com/production/product/original/7f6e4bbc-69a4-4910-a424-8b4f14100c12.jpeg?w=1200"
+        alt="Monster Hunter Poster"
+      />
+      <p>
+        Monster Hunter World is an action role-playing game developed and
+        published by Capcom. A part of the Monster Hunter series, it was
+        released worldwide for PlayStation 4 and Xbox One in January 2018, with
+        a Microsoft Windows version following in August 2018. In the game, the
+        player takes the role of a Hunter, tasked to hunt down and either kill
+        or trap monsters that roam in one of several environmental spaces. If
+        successful, the player is rewarded through loot consisting of parts from
+        the monster and other elements that are used to craft weapons and armor,
+        amongst other equipment. The game's core loop has the player crafting
+        appropriate gear to be able to hunt down more difficult monsters, which
+        in turn provide parts that lead to more powerful gear. Players may hunt
+        alone or in a group of up to four players via the game's online
+        multiplayer.
+      </p>
+
+      <h2>Monster Hunter Cookbook</h2>
+      <img
+        src="https://s.pacn.ws/1500/wn/monster-hunter-recipes-book-587661.1.jpg?pp9r45"
+        alt="Monster Hunter Cookbook"
+      />
+      <p>
+        From the worldwide beloved universe of Monster Hunter comes very
+        tasteful recipes such as Monster Meat, Receptionist lunch-set, and many
+        others. Now all the special 29 recipes are included in this cookbook!
+        Created by the 3-star chefs of the Monster Hunter Café, now you can
+        taste the real flavors of the New World at home! It includes Monster
+        Meat G/Receptionist lunch-set (Paella・Churrasco)/Potion & Potion
+        Great/Alcoholic jelly and Dragon ham salad/Dragon mix hamburg steak/Full
+        baby ice etc.
+      </p>
+    </div>
+  );
+};
+
+export default About;
